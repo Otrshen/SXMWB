632cec02f1b669d934ff64d10968f6f15fb132c7
申铭 <569818710@qq.com>
2017-01-08 19:56:03 +0800

    部署项目

---
+++ SXMWeibo/SXMWeibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SXMWeibo
+//
+//  Created by 申铭 on 17/1/8.
+//  Copyright © 2017年 shenming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
