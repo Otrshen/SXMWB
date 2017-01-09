28064526002f0b6af35dcea532b28bc18c854591
申铭 <569818710@qq.com>
2017-01-09 21:30:07 +0800

    动态加载控制器

--- SXMWeibo/SXMWeibo/Classes/Main/MainViewController.swift
+++ SXMWeibo/SXMWeibo/Classes/Main/MainViewController.swift
@@ -19,19 +19,45 @@ class MainViewController: UITabBarController {
     }
     
     func addChildViewControllers() {
-//        addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
-//        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
-//        addChildViewController(DiscoverTableViewController(), title: "发现", imageName: "tabbar_discover")
-//        addChildViewController(ProfileTableViewController(), title: "我", imageName: "tabbar_profile")
+        guard let filePath = NSBundle.mainBundle().pathForResource("MainVCSettings.json", ofType: nil) else {
+            return
+        }
         
-        addChildViewController("HomeTableViewController", title: "首页", imageName: "tabbar_home")
-        addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
-        addChildViewController("DiscoverTableViewController", title: "发现", imageName: "tabbar_discover")
-        addChildViewController("ProfileTableViewController", title: "我", imageName: "tabbar_profile")
+        guard let data = NSData(contentsOfFile: filePath) else {
+            return
+        }
+        
+        /*
+        Swift和OC不太一样, OC中一般情况如果发生错误会给传入的指针赋值, 而在Swift中使用的是异常处理机制
+        1.以后但凡看大 throws的方法, 那么就必须进行 try处理, 而只要看到try, 就需要写上do catch
+        2.do{}catch{}, 只有do中的代码发生了错误, 才会执行catch{}中的代码
+        3. try  正常处理异常, 也就是通过do catch来处理
+        try! 告诉系统一定不会有异常, 也就是说可以不通过 do catch来处理
+        但是需要注意, 开发中不推荐这样写, 一旦发生异常程序就会崩溃
+        如果没有异常那么会返回一个确定的值给我们
+        
+        try? 告诉系统可能有错也可能没错, 如果没有系统会自动将结果包装成一个可选类型给我们, 如果有错系统会返回nil, 如果使用try? 那么可以不通过do catch来处理
+        */
+        do {
+            let objc = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as! [[String : AnyObject]]
+            
+            for dict in objc {
+                let title = dict["title"] as? String
+                let vcName = dict["vcName"] as? String
+                let imageName = dict["imageName"] as? String
+                addChildViewController(vcName, title: title, imageName: imageName)
+            }
+        } catch {
+            // 只要try对应的方法发生了异常, 就会执行catch{}中的代码
+            addChildViewController("HomeTableViewController", title: "首页", imageName: "tabbar_home")
+            addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
+            addChildViewController("DiscoverTableViewController", title: "发现", imageName: "tabbar_discover")
+            addChildViewController("ProfileTableViewController", title: "我", imageName: "tabbar_profile")
+        }
+
     }
     
-//    func addChildViewController(childController: UIViewController, title : String, imageName : String) {
-    func addChildViewController(childControllerName: String, title : String, imageName : String) {
+    func addChildViewController(childControllerName: String?, title : String?, imageName : String?) {
         
         // 动态获取命名空间
         guard let name =  NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as? String else {
@@ -39,7 +65,10 @@ class MainViewController: UITabBarController {
         }
         
         // 创建类名
-        let cls : AnyObject? = NSClassFromString(name + "." + childControllerName)
+        var cls : AnyObject? = nil
+        if let vcName = childControllerName {
+            cls = NSClassFromString(name + "." + vcName)
+        }
         
         guard let typeCls = cls as? UITableViewController.Type else {
             return
@@ -48,8 +77,11 @@ class MainViewController: UITabBarController {
         let childController = typeCls.init()
         
         childController.title = title
-        childController.tabBarItem.image = UIImage(named: imageName)
-        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        
+        if let ivName = imageName {
+            childController.tabBarItem.image = UIImage(named: ivName)
+            childController.tabBarItem.selectedImage = UIImage(named: ivName + "_highlighted")
+        }
         
         // 包装导航控制器
         let nav = UINavigationController(rootViewController: childController)
