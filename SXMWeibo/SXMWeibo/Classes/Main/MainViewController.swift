5d1a01fd867232a794e475817060f0acdc0bb6d1
申铭 <569818710@qq.com>
2017-01-09 20:43:29 +0800

    命名空间

--- SXMWeibo/SXMWeibo/Classes/Main/MainViewController.swift
+++ SXMWeibo/SXMWeibo/Classes/Main/MainViewController.swift
@@ -19,13 +19,34 @@ class MainViewController: UITabBarController {
     }
     
     func addChildViewControllers() {
-        addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
-        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
-        addChildViewController(DiscoverTableViewController(), title: "发现", imageName: "tabbar_discover")
-        addChildViewController(ProfileTableViewController(), title: "我", imageName: "tabbar_profile")
+//        addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
+//        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
+//        addChildViewController(DiscoverTableViewController(), title: "发现", imageName: "tabbar_discover")
+//        addChildViewController(ProfileTableViewController(), title: "我", imageName: "tabbar_profile")
+        
+        addChildViewController("HomeTableViewController", title: "首页", imageName: "tabbar_home")
+        addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
+        addChildViewController("DiscoverTableViewController", title: "发现", imageName: "tabbar_discover")
+        addChildViewController("ProfileTableViewController", title: "我", imageName: "tabbar_profile")
     }
     
-    func addChildViewController(childController: UIViewController, title : String, imageName : String) {
+//    func addChildViewController(childController: UIViewController, title : String, imageName : String) {
+    func addChildViewController(childControllerName: String, title : String, imageName : String) {
+        
+        // 动态获取命名空间
+        guard let name =  NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as? String else {
+            return
+        }
+        
+        // 创建类名
+        let cls : AnyObject? = NSClassFromString(name + "." + childControllerName)
+        
+        guard let typeCls = cls as? UITableViewController.Type else {
+            return
+        }
+    
+        let childController = typeCls.init()
+        
         childController.title = title
         childController.tabBarItem.image = UIImage(named: imageName)
         childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
